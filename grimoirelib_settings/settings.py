1128cc0d5bf7672735994c87b6071426cdafbd92
Daniel Izquierdo <dizquierdo@bitergia.com>
2015-07-02 00:11:38 +0200

    Add a list of initial constants

---
+++ grimoirelib_settings/settings.py
@@ -0,0 +1,31 @@
+## Copyright (C) 2014 Bitergia
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+## GNU General Public License for more details. 
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+##
+## Authors:
+##   Daniel Izquierdo-Cortazar <dizquierdo@bitergia.com>
+##
+
+
+class GL_Settings(object):
+    """ This class lists a set of constants to be used accross several programs
+    """
+
+    SCM_METATABLE_NAME = "scm_metadata"
+
+    ITS_METATABLE_NAME = "its_metadata"
+
+    SCR_METATABLE_NAME = "scr_metadata"
+
